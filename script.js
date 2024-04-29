74fd19cd1e0fd3a459aec9d841b672887d215557
Alfathey <asycarye@gmail.com>
2024-04-29 11:00:29 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const tokoInfoDiv = document.getElementById('info-toko');
+const bukuTable = document.getElementById('tabel-buku');
+const bukuTableBody = bukuTable.querySelector('tbody');
+
+const fetchBukuData = () => {
+    fetch('toko_buku.json')
+        .then(response => response.json())
+        .then(data => {
+            const { namaToko, pengarang, alamat, buku } = data;
+
+            tokoInfoDiv.innerHTML = `
+                <h2>${namaToko}</h2>
+                <p>Oleh: ${pengarang}</p>
+                <p>Alamat: ${alamat}</p>
+            `;
+
+            buku.forEach(buku => {
+                const row = bukuTableBody.insertRow();
+                row.insertCell().textContent = buku.judul;
+                row.insertCell().textContent = buku.tahunTerbit;
+            });
+        });
+};
+
+fetchBukuData();
